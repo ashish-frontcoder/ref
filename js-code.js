b7f8cc4e88aa67c4a36173962f135678f6698673
ashish-frontcoder <80951019+ashish-frontcoder@users.noreply.github.com>
2023-12-04 07:57:20 +0530

    Update js-code.js

--- js-code.js
+++ js-code.js
@@ -1,3 +1,17 @@
+//Chnage theme color from ligh mode to dark mode
+var icon = document.getElementById("icon");
+icon.onclick = function(){
+    document.body.classList.toggle("dark-theme");
+    if(document.body.classList.contains("dark-theme")){
+        icon.src = "images/sun.png";
+    }
+    else{
+        icon.src = "images/moon.png";
+    }
+}
+
+
+
 // Onclick scroll top smooth
 $(".toTop").click(function (e) {
     e.preventDefault();
